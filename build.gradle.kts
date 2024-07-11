2631294542383c2a57e2de963e95946bda242a1b
Matouš Kučera <mk@kcra.me>
2024-07-11 09:27:10 +0200

    fix: typings source type

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.blossom)
 }
 
-val thisVersion = "0.1.0"
+val thisVersion = "0.1.1"
 
 group = "run.slicer"
 version = "$thisVersion-${libs.versions.vineflower.get()}"
--- vf.d.ts
+++ vf.d.ts
@@ -2,7 +2,7 @@ declare module "@run-slicer/vf" {
     export type Options = Record<string, string>;
 
     export interface Config {
-        source?: (name: string) => Promise<string | null>;
+        source?: (name: string) => Promise<Uint8Array | null>;
         resources?: string[];
         options?: Options;
     }
