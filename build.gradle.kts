98eba7b15abcc963c4c0126d268af6a6f994c681
Matouš Kučera <mk@kcra.me>
2026-05-14 01:18:36 +0200

    chore: downgrade TeaVM to 973b78a2b3510a0bf65341e4edcda3c06093735f

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.teavm) // order matters?
 }
 
-val thisVersion = "0.6.2"
+val thisVersion = "0.6.3"
 
 group = "run.slicer"
 version = "$thisVersion-${libs.versions.vineflower.get()}"
